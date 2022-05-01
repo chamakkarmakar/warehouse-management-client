2e8ef1eed1d694d6a1facecfd9b09f8e9e0cd94a
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-05-01 16:24:32 +0600

    login & register authentication

--- src/Pages/HomePage/Product/Product.js
+++ src/Pages/HomePage/Product/Product.js
@@ -18,7 +18,7 @@ const Product = ({ product }) => {
             </div>
             <div className="px-6 pt-4 pb-2">Price:
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{price}</span>
-                <button onClick={()=> navigateToProductDetails(id)} class="bg-rose-500 hover:bg-rose-800 text-white font-bold py-2 px-4 rounded-full">
+                <button onClick={()=> navigateToProductDetails(id)} className="bg-rose-500 hover:bg-rose-800 text-white font-bold py-2 px-4 rounded-full">
                     GO TO
                 </button>
             </div>
--- src/Pages/LoginRegister/Register/Register.js
+++ src/Pages/LoginRegister/Register/Register.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import './Register.css';
 
 const Register = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const nameRef = useRef('');
+
     const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const [
         createUserWithEmailAndPassword,
         user,
@@ -19,39 +21,41 @@ const Register = () => {
         navigate('/login');
     }
     if (user) {
-        alert('register');
+        navigate('/home');
     }
 
     const handleRegister = e => {
         e.preventDefault();
-        const name = e.target.name.value;
-        const email = e.target.email.value;
-        const password = e.target.password.value;
+        const name= nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
 
         createUserWithEmailAndPassword( email, password);
     }
     return (
-        <div id="login-box">
-            <div className="left">
-                <h1>Sign up</h1>
-
-                <form onSubmit={handleRegister}>
-                    <input type="text" name="username" placeholder="Username" required />
-                    <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="E-mail" required />
-                    <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-                    <input type="submit" name="signup_submit" value="Sign me up" />
-                </form>
-                <p>Already have an Account? Please <span className='cursor-pointer text-blue-500' onClick={navigateLogin}>Sign in</span></p>
+        <div className="container mx-auto mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-xl shadow-2xl">
+            <h1 className='text-2xl font-bold pt-7'>SIGN UP</h1>
+            <form onSubmit={handleRegister}>
+                <div className='mt-8'>
+                    <input className="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" ref={nameRef} type="text" name='name' placeholder="User Name" />
+                </div>
+                <div className='mt-8'>
+                    <input className="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" ref={emailRef} type="email" name='email' placeholder="@gmail.com" required/>
+                </div>
+                <div className="mt-8">
+                    <input className="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" ref={passwordRef} type="password" name='password' placeholder="*************" required/>
+                </div>
+                <div className="mt-10">
+                    <button className="bg-indigo-500 text-gray-100 p-4 w-full rounded-full tracking-wide
+                                font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
+                                shadow-lg">
+                        Sign Up
+                    </button>
+                </div>
+            </form>
+            <div className="mt-12 pb-7 text-sm font-display font-semibold text-gray-700 text-center">
+                Already have an account ? <span onClick={navigateLogin} className="cursor-pointer text-indigo-600 hover:text-indigo-800">Sign in</span>
             </div>
-
-            <div className="right">
-                <span className="loginwith">Sign in with<br />social network</span>
-
-                <button className="social-signin facebook">Log in with facebook</button>
-                <button className="social-signin twitter">Log in with Twitter</button>
-                <button className="social-signin google">Log in with Google+</button>
-            </div>
-            <div className="or">OR</div>
         </div>
     );
 };
